631d165b1fde6dbac77855f908bfe8f842e50fc1
Nicholas Irawan <nicholas2815@hotmail.com>
2025-04-13 02:40:21 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103022330091
+{
+    class CovidConfig
+    {
+        public class PasienCovid
+        {
+            public string satuan_suhu { get; set; }
+            public int batas_hari_demam { get; set; }
+            public string pesan_ditolak { get; set; }
+            public string pesan_diterima { get; set; }
+
+            public PasienCovid(string satuan_suhu, int batas_hari_demam, string pesan_ditolak, string pesan_diterima)
+            {
+                this.satuan_suhu = satuan_suhu;
+                this.batas_hari_demam = batas_hari_demam;
+                this.pesan_ditolak = pesan_ditolak;
+                this.pesan_diterima = pesan_diterima;
+            }
+        }
+
+        public PasienCovid pasienCovid;
+
+        public const String filePath = @"covid_config.json";
+
+        public CovidConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            } 
+            catch (Exception e)
+            {
+                WriteNewConfigFile();
+            }
+        }
+
+        private PasienCovid ReadConfigFile()
+        {
+            String configJson = File.ReadAllText(filePath);
+            pasienCovid = JsonSerializer.Deserialize<PasienCovid>(configJson);
+            return pasienCovid;
+        }
+
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String json = JsonSerializer.Serialize(pasienCovid, options);
+            File.WriteAllText("covid_config.json", json);
+        }
+
+        public void ubahSatuan()
+        {
+            if (pasienCovid.satuan_suhu.Equals("celsius"))
+            {
+                pasienCovid.satuan_suhu = "fahrenheit";
+            }
+            else
+            {
+                pasienCovid.satuan_suhu = "celsius";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul8_103022330091;
+
+class main
+{
+    public static void Main(string[] args)
+    {
+        int derajat = -1;
+        int perkiraanHari = -1;
+        CovidConfig covidConfig = new CovidConfig();
+
+        covidConfig.ubahSatuan();
+
+        Console.WriteLine($"Berapa suhu badan anda saat \r\nini? Dalam nilai {covidConfig.pasienCovid.satuan_suhu}");
+        String input = Console.ReadLine();
+        try
+        {
+            derajat = int.Parse(input);
+        } 
+        catch (Exception e)
+        {
+            Console.WriteLine("input salah");
+        }
+
+        Console.WriteLine("Berapa hari yang lalu(perkiraan) anda terakhir memiliki gejala deman ?");
+        input = Console.ReadLine();
+        try
+        {
+            perkiraanHari = int.Parse(input);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("input salah");
+        }
+
+        if (check(covidConfig, derajat, perkiraanHari))
+        {
+            Console.WriteLine(covidConfig.pasienCovid.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(covidConfig.pasienCovid.pesan_ditolak);
+        }
+    }
+
+    private static bool check(CovidConfig config, int suhu, int perkiraanHari)
+    {
+        if (config.pasienCovid.satuan_suhu.Equals("celsius"))
+        {
+            return suhu > 36.5 && suhu < 37.5 && perkiraanHari < config.pasienCovid.batas_hari_demam;
+        }
+        else
+        {
+            return suhu > 97.7 && suhu < 99.5 && perkiraanHari < config.pasienCovid.batas_hari_demam;
+        }
+    }
+}
